be135d846ea759c9daf5ff659e14389574b10a5f
ArsalaBangash <arsalabangash@gmail.com>
2021-02-21 15:27:32 +0500

    Modified theme import

--- packages/create-nuxt-content-docs/template/nuxt.config.js
+++ packages/create-nuxt-content-docs/template/nuxt.config.js
@@ -1,7 +1,22 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from 'nuxt-content-docs-theme-grey'
 
 export default theme({
-  docs: {
-    primaryColor: '#E24F55'
-  }
+    components: true,
+    content: {
+        liveEdit: false
+    },
+    docs: {
+        primaryColor: '#64748b'
+    },
+    head: {
+        script: [
+            {
+                src: 'https://plausible.io/js/plausible.js',
+                async: true,
+                defer: true,
+                'data-domain': '',
+            },
+        ]
+    }
 })
+
