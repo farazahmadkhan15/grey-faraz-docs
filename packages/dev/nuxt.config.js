672b33b623a6ad25e996781c27c50ab3a0e49ec4
Adil Shehzad <muhammadadil@ucp.edu.pk>
2021-02-22 16:39:50 +0000

    Adding Gitlab Logo

---
+++ packages/dev/nuxt.config.js
@@ -0,0 +1,22 @@
+import theme from 'nuxt-content-docs-theme-grey'
+
+export default theme({
+    components: true,
+    content: {
+        liveEdit: false
+    },
+    docs: {
+        primaryColor: '#64748b'
+    },
+    head: {
+        script: [
+            {
+                src: 'https://plausible.io/js/plausible.js',
+                async: true,
+                defer: true,
+                'data-domain': '',
+            },
+        ]
+    }
+})
+
