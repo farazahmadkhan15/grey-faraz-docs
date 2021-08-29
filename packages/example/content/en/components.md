4be1f1e54c0de752a282cf9e4f97eacf096bd2f1
farazahmadkhan <farazahmadkhan15@gmail.com>
2021-08-29 12:57:19 +0500

    Props added

--- packages/example/content/en/components.md
+++ packages/example/content/en/components.md
@@ -9,12 +9,15 @@ category: Guide
 
 <author name="Arsala" desc="President at Grey Software" linkedin-url="https://linkedin.com/in/ArsalaBangash" twitter="arsalagrey" avatar-url="https://gitlab.com/uploads/-/system/user/avatar/2274539/avatar.png" gitlab-url="https://gitlab.com/ArsalaBangash" github-url="https://github.com/ArsalaBangash" ></author>
 
+
 ### CTA Button
 
 <cta-button text="Explore" link="https://ecosystem.grey.software">
 </cta-button>
 
 ### Sponser Card
-<SponserCard></SponserCard>
+<SponsorCard name="ArsalaBangash" amount="$20/Month" sponsor-since="Sponsor Since Augest 2021" github-url= "https://github.com/ArsalaBangash" ></SponsorCard>
+
+
 
 <br></br>
--- packages/grey-docs/src/components/global/SponsorCard.vue
+++ packages/grey-docs/src/components/global/SponsorCard.vue
@@ -1,40 +1,49 @@
 <template>
-  <div class="flex w-2/3 bg-white font-sans dark:bg-gray-800 px-8 rounded-xl shadow">
+  <div class="flex w-2/3 bg-white font-sans dark:bg-gray-800 px-6 py-2 mb-8 rounded-xl shadow-xl ">
     <div class="flex-auto px-2">
       <div class="flex flex-wrap items-center">
         <p
           class="flex-auto text-3xl font-semibold text-gray-700 dark:text-white"
         >
-          Faraz Ahmad Khan
+        {{ name }}
+        
         </p>
         <div class="text-xs text-gray-500 text-gray-500 dark:text-gray-500">
-          $110.00/Month
+          {{ amount }}
         </div>
         <div
           class="
             w-full
             flex-none
-            -mt-4
+            -mt-6
             font-extralight
             text-sm text-grey-700
             dark:text-gray-300
           "
         >
-          Sponser since Augest 2014
+        {{ sponsorSince }}
+          
         </div>
       </div>
-      <div class="flex items-center">
-        <span class="relative pr-2">
-          <IconGithub class="h5 w-5" />
+      <div class="flex py-2 items-center">
+        <span class="relative pr-1">
+          <IconGithub class="h-4 w-4" />
         </span>
-        <p class="text-md text-black dark:text-white">Github</p>
+        <a :href="githubUrl" class="text-md text-black dark:text-white">Github</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    name: String,
+    amount: String,
+    sponsorSince: String,
+    githubUrl: String,
+  },
+};
 </script>
 
 <style>
